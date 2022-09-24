38ca103a127047796ec3f8b68136d55b7afa82d7
Baitwo02 <baitwo02@qq.com>
2022-09-24 19:04:49 +0800

    add light control function

---
+++ scripts/dwm-status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+    /bin/bash ~/suckless/dwm/scripts/dwm-status-refresh.sh
+    sleep 1
+done
+
--- scripts/sh-scripts/dwm-status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    /bin/bash ~/suckless/dwm/scripts/sh-scripts/dwm-status-refresh.sh
-    sleep 1
-done
-
